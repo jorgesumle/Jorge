7b4e14f2f30ee5f84f5affabff1a383e7c09de93
Jorge <jorgesumle@freakspot.net>
2016-07-25 14:47:29 +0200

    Improved fullscreeen to allow toggling between fullscreen and normal mode and renamed the title of the empty window program and changed its name to empty_window.py

--- empty_window.py
+++ empty_window.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 game_icon = pygame.image.load('icon.png')
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((200, 300))
-pygame.display.set_caption('Simple ventana')
+pygame.display.set_caption('Empty window')
 pygame.display.set_icon(game_icon)
 while True:
     for event in pygame.event.get():
--- fullscreen.py
+++ fullscreen.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python
 import pygame
-import sys
 from pygame.locals import *
 
-game_icon = pygame.image.load('icon.png')
+HEIGHT = 700
+WIDTH = 700
+
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((200, 300))
-pygame.display.set_caption('Simple ventana')
-pygame.display.set_icon(game_icon)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Fullscreen demo')
 
-if pygame.display.get_driver()=='x11':
-    pygame.display.toggle_fullscreen()
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 
-while True:
+#Creates the text
+font = pygame.font.Font('freesansbold.ttf', 22)
+textSurfaceObj = font.render('Press F11 to change between full screen and normal mode', True, WHITE, GREEN)
+textRectObj = textSurfaceObj.get_rect()
+textRectObj.center = (WIDTH / 2, HEIGHT / 2)
+
+#Draws the text
+screen.blit(textSurfaceObj, textRectObj)
+
+fullscreen = False
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
-
-
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_F11:
+                if pygame.display.get_driver() == 'x11': 
+                    pygame.display.toggle_fullscreen()
+                else:
+                    screen_copy = screen.copy()
+                    if fullscreen:
+                        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+                    else:
+                        screen = pygame.display.set_mode((WIDTH , HEIGHT), pygame.FULLSCREEN)
+                    fullscreen = not fullscreen
+                    screen.blit(screen_copy, (0, 0))
+            elif event.key == K_ESCAPE:
+                running = False
     pygame.display.update()
 
